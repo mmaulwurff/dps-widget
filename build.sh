f3782fa11f2bb3d4f0dd67c4f396315a138886cc
Alexander Kromm <mmaulwurff@gmail.com>
2020-12-17 00:19:22 +0700

    add version to package build

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-#filename=dps-widget-$(git describe --abbrev=0 --tags).pk3
-filename=dps-widget.pk3
+filename=dps-widget-$(git describe --abbrev=0 --tags).pk3
 
 rm -f $filename
 zip -R $filename "*.md" "*.txt" "*.zs" "*.png"
